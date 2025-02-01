7062512c48c2063ca43802ac6db31f32ef611dbf
Art-Luzrab <arthur91021@gmail.com>
2025-01-31 16:26:28 -0800

    How to NOT use Fetch Data in React

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import App from "./App";
 import StarRating from "./StarRating";
 import { useState } from "react";
+import "./index.css";
 
 function Test() {
   const [movieRating, setMovieRating] = useState(0);
@@ -18,13 +18,11 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
-
-    <Test />
+    <StarRating size={24} color="red" className="test" defaultRating={3} /> */}
   </React.StrictMode>
 );
